48d143b640e4305a71281b23d31fbf8b257885f7
sahel <sahel22.vis@gmail.com>
2024-08-12 08:18:50 +0530

    packages added

--- Client/src/pages/Signin/Signin.jsx
+++ Client/src/pages/Signin/Signin.jsx
@@ -5,12 +5,13 @@ export default function Signin() {
             <div className='info'>
                 <div className='infocontent'>
                     <h1 id='heading'>Sign In</h1>
-                    <input type='text' id="inputSignIn" placeholder='Username'></input> 
-                    <input type='password' id="inputSignIn" placeholder='Password'></input>
-                    <div className='buttonSignin'>
-                        <button id='signin'>SIGN IN</button> 
-                    </div>
-                    
+                    <form>
+                        <input type='text' id="inputSignIn" placeholder='Username'></input> 
+                        <input type='password' id="inputSignIn" placeholder='Password'></input>
+                        <div className='buttonSignin'>
+                            <button id='signin'>SIGN IN</button> 
+                        </div>
+                    </form>
                 </div>
 
             </div>
